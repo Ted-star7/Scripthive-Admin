11d45dad25848f74fd6cc17af5ae55303d48b39c
RAYSONBARAKA <barakarayson6@gmail.com>
2025-07-16 12:25:39 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.VITE_BASE_PATH || "/Script", // Base path should be at the top level
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean), // Remove false values
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
