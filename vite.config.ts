db12bbebe1a4bf59d42c6e4687639a1912c31f59
Teddy Kibuthu <kibuthuteddy@gmail.com>
2025-07-17 00:54:35 +0300

    tnt

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
+  base: '/', // Add this line
   server: {
     host: true,
     port: 8080,
     strictPort: true,
     open: '/login',
   },
+
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
